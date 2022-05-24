d0160957fbc72973efc55cd7044cc85ecce0b3ad
Chris Simpson <chris.m.simpson@icloud.com>
2022-05-24 12:45:20 +1000

    -

---
+++ Bootstrap/Core/Tokenizer.cpp
@@ -0,0 +1,13 @@
+
+#include "Tokenizer.h"
+
+namespace Core {
+        
+    Tokenizer::Tokenizer(/* args */) {
+
+    }
+
+    Tokenizer::~Tokenizer() {
+
+    }
+}
---
+++ Bootstrap/Core/Tokenizer.h
@@ -0,0 +1,21 @@
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+namespace Core {
+
+    class Tokenizer {
+
+    public:
+
+        Tokenizer(/* args */);
+
+        ~Tokenizer();
+        
+    private:
+
+        /* data */
+    };
+}
+
+#endif
